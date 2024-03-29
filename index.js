bb98d090eee3772c64651de803498bd564e1588e
naveendoddi <prasadnaveen847@gmail.com>
2024-03-29 09:24:32 +0530

    pushing mongodb

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-const jsonServer = require('json-server')  
-const path = require('path')
-const server = jsonServer.create()  
-const router = jsonServer.router('db.json')  
-const middlewares = jsonServer.defaults()  
 
-server.use(middlewares)  
-server.use(router)  
-server.listen(3000, () => {  
-  console.log('JSON Server is running')  
-})
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = "mongodb+srv://naveendoddi:zQTrjUrwyKXeIEZ2@swiggy.jbdpwef.mongodb.net/?retryWrites=true&w=majority&appName=swiggy";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
